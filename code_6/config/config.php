5297196b47608acfc28ccec9c557f6cc50916a34
Asala NASRI <asala.nasri@edu.univ-paris13.fr>
2026-05-05 22:24:42 +0200

    Suppression des fichiers indésirables

--- code_6/config/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('API_BASE_URL','http://127.0.0.1:5000');
-?>
--- code_8/config.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    "host": "mysql",
-    "port": 3306,
-    "user": "root",
-    "password": "root",
-    "database": "school_api"
-}
